805e1da590c610097ed300022fd0160ecf132f6a
mlattanzio1 <matt.lattanzio1@gmail.com>
2014-11-05 11:32:03 -0500

    Family Meal Website

---
+++ OLD/scripts.js
@@ -0,0 +1,10 @@
+
+
+$( document ).ready(function() {
+   
+    	$(".flip-container").on("click", function(){
+    		var $flip = $(this);
+    		$(this).toggleClass("flippingAction");
+    	});
+ 
+});
